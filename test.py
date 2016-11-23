44d4fdc15bf80209b6f6df35a97b577146a9968c
Henrique Silveira <hd2312@columbia.edu>
2016-11-22 22:44:44 -0200

    Testing dicttoxml module in subdirectory and adding test file to create xml from dict keeping the encoding in the XML header

---
+++ test.py
@@ -0,0 +1,9 @@
+from libs import dicttoxml
+from xml.dom.minidom import parseString
+
+dict = {'Nome': 'HighEnd', 'Age': 65, 'Class': 'Super Professor'}
+
+xml = parseString(dicttoxml.dicttoxml(dict)).toprettyxml(encoding="UTF-8")
+
+with open('instance.xml', 'w') as f:
+    f.write(xml)
