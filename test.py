2d224eefc26d4056efd698afb859a01ddceb0761
Adriano Belfort <adriano.belfort@outlook.com>
2016-11-22 22:42:05 -0200

    Added attr_type=False to dicttoxml method

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from xml.dom.minidom import parseString
 
 dict = {'Nome': 'HighEnd', 'Age': 65, 'Class': 'Super Professor'}
 
-xml = parseString(dicttoxml.dicttoxml(dict)).toprettyxml(encoding="UTF-8")
+xml = parseString(dicttoxml.dicttoxml(dict, attr_type=False)).toprettyxml(encoding="UTF-8")
 
 with open('instance.xml', 'w') as f:
     f.write(xml)
